c6619c4c96ebb0392c8bbb3ec866cc56bb50b05f
dgavrichkov <dgavrichkov32@gmail.com>
2019-04-30 13:42:42 +0300

    файлы перенесены в корень проекта

--- app/scripts/scripts.js
+++
@@ -1,6 +0,0 @@
-var trigger = document.querySelector('#burger');
-var popup = document.querySelector('#mainmenu');
-
-trigger.addEventListener('click', function(evt){
-    popup.classList.toggle('menu--open');
-})
